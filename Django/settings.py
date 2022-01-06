ccd552246a8b4e6230d064a4b8890ae90dcc1681
SpiritusMalus <Leos2111@gmail.com>
2022-01-06 19:22:49 +0300

    Lesson 4. Code style fix#2

--- Django/settings.py
+++ Django/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+f@ga!@kdpvvn0*dp(^c@6lt$)l9aqtz4*zf@=k1d%z8%^j5hk"
+SECRET_KEY = (
+    "django-insecure-+f@ga!@kdpvvn0*dp(^c@6lt$)l9aqtz4*zf@=k1d%z8%^j5hk"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
